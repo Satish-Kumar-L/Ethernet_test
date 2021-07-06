96c827566a6c1a110d3ce26ed0ea9207df19d52d
Satish-Kumar-L <72338697+Satish-Kumar-L@users.noreply.github.com>
2021-07-06 14:18:23 +0530

    Create udp_client_string.py

---
+++ udp_client_string.py
@@ -0,0 +1,35 @@
+import socket
+import time
+import matplotlib.pyplot as plt
+
+Server_add = "192.168.1.2"
+Server_port = 20001
+serverAddressPort   = (Server_add,Server_port)
+
+msgFromClient       = "123456789"
+array_len = len(msgFromClient)
+print(array_len)
+Int32Array = c_uint32 * array_len
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+latency = []
+iterator = 0
+
+while iterator <= 1000:
+
+    temp1 =  time.clock()
+    UDPClientSocket.sendto(Int32Array(*msgFromClient), serverAddressPort)
+    msgFromServer = UDPClientSocket.recvfrom(1024)
+    latency.append((time.clock()-temp1))
+    print('server:')
+    print(list(Int32Array.from_buffer_copy(msgFromServer[0])))
+    iterator += 1
+    
+
+fig = plt.figure()
+plt.hist(latency,bins=20)
+print('Latency histogram ('+str(array_len*4)+')')
+fig.suptitle('Latency histogram ('+str(array_len*4)+' bytes)')
+plt.xlabel('Latency time(s)')
+plt.ylabel('Frequency')
+plt.show()
