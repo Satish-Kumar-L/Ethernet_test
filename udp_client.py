ac5669d59b977b42ed5c10ab1ac90fde09d62c91
Satish-Kumar-L <72338697+Satish-Kumar-L@users.noreply.github.com>
2021-07-21 18:55:06 +0530

    Update udp_client.py

--- udp_client.py
+++ udp_client.py
@@ -1,37 +1,44 @@
 import socket
 import time
 import matplotlib.pyplot as plt
-import struct
-from ctypes import *
+import math
+import keyboard
 
-Server_add = "192.168.1.2"
-Server_port = 20001
-serverAddressPort   = (Server_add,Server_port)
-
-msgFromClient       = list(range(0,64))
+msgFromClient       = 'h'*256
 array_len = len(msgFromClient)
-print(array_len)
-Int32Array = c_uint32 * array_len
+bytesToSend         = str.encode(msgFromClient)
 
+serverAddressPort   = ("192.168.1.2", 20001)
+
+bufferSize = 1024
+packets_sent = 0
+packets_recieved = 0
+ 
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 latency = []
-iterator = 0
-
-while iterator <= 1000:
-
-    temp1 =  time.clock()
-    UDPClientSocket.sendto(Int32Array(*msgFromClient), serverAddressPort)
-    msgFromServer = UDPClientSocket.recvfrom(1024)
-    latency.append((time.clock()-temp1))
-    print('server:')
-    print(list(Int32Array.from_buffer_copy(msgFromServer[0])))
-    iterator += 1
+t_start = time.time()
+while packets_sent<1000:
     
-
+    temp1 =  time.clock()
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    latency.append((time.clock()-temp1)*1000000)
+    
+    print(msg)
+ 
+    packets_sent += 1
+    if(bytesToSend == msgFromServer[0]):
+        packets_recieved += 1
+    
+t_end = time.time()
+print("test time: "+str((t_end-t_start)/60)+" mins")
+print("total packets sent: "+str(packets_sent))
+print("total packets recieved: "+str(packets_recieved))
+print("packet loss: "+str((packets_sent-packets_recieved)/packets_sent))
+print("throughput: "+str(((packets_sent+packets_recieved)*array_len*0.001)/(math.fsum(latency)/1000000))+" kBps")
 fig = plt.figure()
 plt.hist(latency,bins=20)
-print('Latency histogram ('+str(array_len*4)+')')
-fig.suptitle('Latency histogram ('+str(array_len*4)+' bytes)')
-plt.xlabel('Latency time(s)')
+fig.suptitle('Latency histogram ('+str(array_len)+' bytes)')
+plt.xlabel('Latency time(us)')
 plt.ylabel('Frequency')
 plt.show()
